682322369073811e687d049cb3cc93295c4fd0da
George Go <george.js.gosieski@gmail.com>
2015-10-09 19:31:08 -0700

    Added a volume slider in a form along with basic volume controls and consistency when starting other songs; Still an issue with the slider not registering where it finishes if the user let's go of the mouse outisde of the slider

--- pho-add-content.js
+++ pho-add-content.js
@@ -1,5 +1,8 @@
 var source;
 var song = new Audio();
+var volume = 1;
+
+document.getElementById('volumeControl').elements['volumeSlider'].defaultValue = 100;
 
 function playSong(songName){
     if(!(song.paused||song.muted||song.ended)){
@@ -7,5 +10,13 @@ function playSong(songName){
     }
     source = 'music/'+songName+'.mp3';
     song = new Audio(source);
+    song.volume = volume
     song.play();
+}
+
+function changeVolume(){
+    var volumeSlider = document.getElementById('volumeControl').elements['volumeSlider'];
+    volume = Number(volumeSlider.value)/100;
+    console.log(volume);
+    song.volume = volume;
 }

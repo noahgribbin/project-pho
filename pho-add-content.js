aecdbe89da82f534a0346c50270e3194d8dfa2c6
George Go <george.js.gosieski@gmail.com>
2015-10-11 16:10:36 -0700

    Made a dynamic background based on a canvas tutorial

--- pho-add-content.js
+++ pho-add-content.js
@@ -20,3 +20,35 @@ function changeVolume(){
     console.log(volume);
     song.volume = volume;
 }
+
+
+var canvas = document.createElement("canvas");
+canvas.width=100;
+canvas.height=100;
+
+var ctx = canvas.getContext("2d");
+ctx.clearRect(0,0,100,100);
+var grayPalette = ["#aaaaaa","#bbbbbb","#cccccc","#dddddd","#eeeeee"];
+
+function newBackground(){
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            ctx.beginPath();
+            ctx.rect(0+10*j,0+10*i,10,10);
+            var randomColorIndex = Math.round(Math.random()*(grayPalette.length-1));
+            ctx.fillStyle = grayPalette[randomColorIndex];
+            ctx.fill();
+            ctx.strokeStyle = "#ffffff";
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+}
+
+function setDynamicBackground(){
+    newBackground();
+    var imageDataURL = canvas.toDataURL();
+    document.body.style.background="transparent url('"+imageDataURL+"') repeat";
+}
+
+var timer = setInterval(setDynamicBackground,1000);
